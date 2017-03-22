42ebd92f00214e9dab2da7f9c21bed4438bb6767
jag10 <jota259@gmail.com>
2017-03-22 11:24:26 +0100

    add barrier.c

--- mpi_exec.sh
+++ mpi_exec.sh
@@ -4,5 +4,5 @@ num_proc=$2
 dir=bin
 bin_file=${file/\.c/}
 
-mpicc -std=c99 $file -o $dir/$bin_file && mpiexec -n $num_proc $dir/$bin_file
+mpicc -std=gnu99 $file -o $dir/$bin_file && mpiexec -n $num_proc $dir/$bin_file
 # chmod 744 me
