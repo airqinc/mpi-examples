c9fe68882f4fed320d386354522941dd2b10a670
jag <jota259@gmail.com>
2017-04-02 20:00:00 +0200

    pretty print on barrier.c

--- collective_comm/barrier.c
+++ collective_comm/barrier.c
@@ -8,10 +8,10 @@ int main(int argc, char *argv[]){
   MPI_Comm_size(MPI_COMM_WORLD,&world_size);
   MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
   if(world_rank == 0) {
-    printf("root process: wait for me...\n");
-    sleep(1);
     printf("Hello, world.  I am %d of %d\n", world_rank, world_size);fflush(stdout);
     usleep(1000); //time for IO
+    printf("root process: wait for me...\n");
+    sleep(1);
   } else {
     printf("Hello, world.  I am %d of %d\n", world_rank, world_size);fflush(stdout);
   }
