2f3b6270e2c69354adfd584e287b7619703f7625
jag <jota259@gmail.com>
2017-04-02 13:03:00 +0200

    add exercise for collective communication: prime.c

--- mpi_exec.sh
+++ mpi_exec.sh
@@ -5,5 +5,5 @@ filename=$(basename $1)
 dir=bin
 bin_file=${filename/\.c/}
 
-mpicc -std=gnu99 $file_path -o $dir/$bin_file && mpiexec -n $num_proc $dir/$bin_file
+mpicc -std=gnu99 -lm $file_path -o $dir/$bin_file && mpiexec -n $num_proc $dir/$bin_file
 # chmod +x me
